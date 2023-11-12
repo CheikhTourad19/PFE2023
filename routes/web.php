88829b1a58cbf41ca3dcac0ffaec029b318a4e9b
smallziz18 <abdoulaziz17.diouf@ucad.edu.sn>
2023-11-12 08:57:23 +0000

    reussi

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/', function () {
 
     $annonces = \App\Models\User::join('annonce as a', 'users.id', '=', 'a.idUser')->orderBy('a.created_at', 'asc')
@@ -36,11 +43,5 @@ Route::get('/dashboard', function () {
 
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
