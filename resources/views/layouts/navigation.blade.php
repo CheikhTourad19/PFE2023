e1da31b3ea054525ff19642b8fd89b886e462b85
smallziz18 <abdoulaziz17.diouf@ucad.edu.sn>
2023-11-23 11:05:46 +0000

    verified

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,6 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex" >
+
                     <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
                         {{ __('Profil') }}
                     </x-nav-link>
